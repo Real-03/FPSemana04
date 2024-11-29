e8e871eb39b68950fa96caad1775be0a502df47a
Tomás Real <tomasreal.art3@gmail.com>
2024-11-29 10:24:51 +0000

    Fisrt Commit

---
+++ read_json.py
@@ -0,0 +1,16 @@
+import json
+
+file_path = input()
+
+try:
+
+    file= open(file_path,'rt',encoding='utf-8')
+    json_data=file.read()
+    data = json.loads(json_data)
+    print(data)
+except:
+  print("Ocorreu um erro!")
+
+print("Processo Concluído")
+
+
